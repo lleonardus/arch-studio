c25edf5e8d4c9ec9da38c99c97545bdab162a52a
lleonardus <leonardus.wizard@gmail.com>
2024-10-04 00:17:44 -0300

    fixing bug by changing the max-tablet:overflow-hidden class to max-tablet:overflow-x-hidden

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Header } from "./Header";
 export function AppLayout() {
   return (
     <div className="flex h-screen w-screen justify-center font-spartan">
-      <div className="relative h-full w-full max-tablet:overflow-hidden tablet:max-w-screen-tablet tablet:pl-[97px] desktop:max-w-screen-desktop desktop:pl-[164px]">
+      <div className="relative h-full w-full max-tablet:overflow-x-hidden tablet:max-w-screen-tablet tablet:pl-[97px] desktop:max-w-screen-desktop desktop:pl-[164px]">
         <Header />
         <Outlet />
         <footer></footer>

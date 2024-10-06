dfbca4571397f5d14197fb4b84c3f7350e271a58
lleonardus <leonardus.wizard@gmail.com>
2024-10-05 22:15:07 -0300

    creating about us section on homepage

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import { Hero } from "./components/hero/Hero";
 import { WelcomeSection } from "./components/WelcomeSection";
+import { AboutUsSection } from "./components/AboutUsSection";
 
 export function Homepage() {
   return (
     <main>
       <Hero />
       <WelcomeSection />
+      <AboutUsSection />
     </main>
   );
 }

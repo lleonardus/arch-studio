e006123647253e89bad315874c6d5a4e19aad477
lleonardus <leonardus.wizard@gmail.com>
2024-10-05 22:17:43 -0300

    changing h3 to h2

--- src/pages/homepage/components/AboutUsSection.jsx
+++ src/pages/homepage/components/AboutUsSection.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export function AboutUsSection() {
   return (
     <section className="relative z-[1] mt-[113px] space-y-[23px] bg-black bg-[url('/assets/home/mobile/image-small-team.jpg')] bg-cover bg-no-repeat px-8 py-[180px] after:absolute after:inset-0 after:bg-black/60 tablet:mr-[98px] tablet:mt-[233px] tablet:space-y-10 tablet:bg-[url('/assets/home/tablet/image-small-team.jpg')] tablet:py-[168px] tablet:pl-[58px] tablet:pr-[70px] desktop:mt-[200px] desktop:space-y-10 desktop:bg-[url('/assets/home/desktop/image-small-team.jpg')] desktop:py-[168px] desktop:pl-[190px]">
-      <h3 className="relative z-[2] max-w-[250px] text-[48px] font-bold leading-[52px] tracking-[-1.71px] text-white tablet:max-w-[400px] tablet:text-xl">
+      <h2 className="relative z-[2] max-w-[250px] text-[48px] font-bold leading-[52px] tracking-[-1.71px] text-white tablet:max-w-[400px] tablet:text-xl">
         Small team, big ideas
-      </h3>
+      </h2>
       <Link
         to="/about-us"
         className="relative z-[2] flex w-fit items-center gap-[24px] bg-gray-900 pb-[22px] pl-[37px] pr-[32px] pt-[25px] text-base font-bold capitalize text-white transition-colors hover:bg-gray-700"

d3e2d439bf6a4d5edc2340903112f6fd7dd0c4c1
lleonardus <leonardus.wizard@gmail.com>
2024-10-06 23:57:30 -0300

    removing margins from PortfolioList

--- src/pages/portfolio/components/PortfolioList.jsx
+++ src/pages/portfolio/components/PortfolioList.jsx
@@ -1,6 +1,6 @@
 export function PortfolioList({ children }) {
   return (
-    <ul className="mb-6 mt-[43px] flex flex-col gap-6 tablet:col-span-2 tablet:mb-0 tablet:mt-[85px] desktop:mt-16 desktop:grid desktop:grid-cols-3">
+    <ul className="flex flex-col gap-6 tablet:col-span-2 desktop:grid desktop:grid-cols-3">
       {children}
     </ul>
   );
